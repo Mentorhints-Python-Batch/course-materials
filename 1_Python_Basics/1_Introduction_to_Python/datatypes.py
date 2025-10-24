2a5a88e8d96ec39899f111cea4d0538ee3b5b619
revantharunachalam <revanth.sbioa@gmail.com>
2025-10-24 18:01:47 +0530

    [https://github.com/Mentorhints-Python-Batch/course-materials/issues/3]: launch.json file and sample .py file for execution added.

---
+++ 1_Python_Basics/1_Introduction_to_Python/datatypes.py
@@ -0,0 +1,11 @@
+# Integer
+a = 10
+print("Integer:", a, "Type:", type(a))
+
+# Float
+b = 10.5
+print("Float:", b, "Type:", type(b))
+
+# String
+c = "Hello, Python!"
+print("String:", c, "Type:", type(c))
